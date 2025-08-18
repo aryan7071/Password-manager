9f27e65fc519d49ea853ccaf0e6899bff258433b
aryan7071 <aryan002005@gmail.com>
2025-08-19 01:53:20 +0530

    passop

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import Navbar from './component/Navbar'
+import Manager from './component/Manager'
+
+function App() {
+  return (
+  <>
+  <Navbar/>
+  <Manager/>
+  </>
+  )
+}
+
+export default App
